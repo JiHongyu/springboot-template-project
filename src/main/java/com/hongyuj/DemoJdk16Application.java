0f6d6bcb3374f0a203314c48f902b0e35a649a7b
jihongyu <jhyqingy@gmail.com>
2021-06-12 20:29:19 +0800

    init commit

---
+++ src/main/java/com/hongyuj/DemoJdk16Application.java
@@ -0,0 +1,13 @@
+package com.hongyuj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJdk16Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJdk16Application.class, args);
+    }
+
+}
